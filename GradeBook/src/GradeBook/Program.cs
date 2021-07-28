19763110b21d03b254cca933e04543cb469ffd4e
Pavan Kumar Mangalapally <reddy_546@hotmail.com>
2021-07-27 19:28:00 -0700

    Added List/Arrays

--- GradeBook/src/GradeBook/Program.cs
+++ GradeBook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,11 +7,35 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double[] numbers = new double[3] {11.2, 12.3, 14.5};
+            var result = 0.0;
+            foreach (double number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine($"Sum of grades is {result}");
+
+            List<double> NewNumbers = new List<double>();
+            NewNumbers.Add(39.6);
+            NewNumbers.Add(67.89);
+            NewNumbers.Add(98.9);
+            var newresult = 0.0;
+            
+            foreach(double ListObj in NewNumbers)
+            {
+                newresult += ListObj;
+
+            }
+
+            Console.WriteLine($"New List Total is {newresult}");
+
+            /* Commented after successful testing
             var UserName = "Pavan Kumar";
             Console.WriteLine("Hello World!");
             Console.WriteLine($"Hello World!");
             Console.WriteLine ("Hello " + UserName + "!");
             Console.WriteLine ($"Hello {UserName}!");
+            
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}");
@@ -19,6 +44,7 @@ namespace GradeBook
             {
                 Console.WriteLine ("No argulemnts found");
             }
+            */
 
         }
     }
