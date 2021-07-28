8536b48e467f0a7f45c224ca859cf302925f9c67
Pavan Kumar Mangalapally <reddy_546@hotmail.com>
2021-07-27 18:46:44 -0700

    PluralSight lesson 1!
    PluralSight lesson 1!

---
+++ GradeBook/src/GradeBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var UserName = "Pavan Kumar";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello World!");
+            Console.WriteLine ("Hello " + UserName + "!");
+            Console.WriteLine ($"Hello {UserName}!");
+
+        }
+    }
+}
