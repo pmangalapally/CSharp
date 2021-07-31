1c85d279f3cf57f20b382647f0c259e06698aa4a
Pavan Kumar Mangalapally <reddy_546@hotmail.com>
2021-07-31 06:22:47 -0700

    Tesing code with VS 2019.

---
+++ CustomFileCompression/CustomFileCompression/CustomFileCompression/CompressFiles.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO.Compression;
+using System.IO;
+namespace CustomFileCompression
+{
+    class CompressFiles
+    {
+        public CompressFiles()
+        {
+        }
+
+        static void DeleteDirectory (string directoryPath)
+        {
+            try
+            {
+                if(Directory.Exists(directoryPath))
+                {
+                    Directory.Delete(directoryPath);
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in deleting directory :" + ex.Message);
+            }
+        }
+
+        static void DeleteFile (string srcFilePath)
+        {
+            try
+            {
+                if(File.Exists(srcFilePath))
+                {
+                    File.Delete(srcFilePath);
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in deleting file :" + ex.Message);
+            }
+        }
+
+        public void MoveCompressedFiles(string srcPath, string destPath, double fileWaitTime)
+        {
+            string[] filePathinDir = Directory.GetFiles(srcPath, "*.txt");
+
+            foreach (string filePathName in filePathinDir)
+            {
+                if (((DateTime.Now - File.GetLastAccessTime(filePathName)).TotalSeconds) > fileWaitTime)
+                {
+                    string srcFileName = Path.GetFileName(filePathName);
+                    //Console.WriteLine($"File name {filePathName}");
+                    string compressedFileName = srcFileName + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".zip";
+                    string destFileName = Path.Combine(destPath, compressedFileName);
+                    string tempDir = Path.Combine(srcPath, "tempdirectory");
+                    string MoveFileName = Path.Combine(tempDir, srcFileName);
+
+                    DeleteDirectory(tempDir);
+                    //Console.WriteLine($"Directory deleted");
+
+                    Directory.CreateDirectory(tempDir);
+                    File.Move(filePathName, MoveFileName);
+                    ZipFile.CreateFromDirectory(tempDir, destFileName);
+
+                    DeleteFile(MoveFileName);
+                    DeleteDirectory(tempDir);
+                }
+            }
+        }
+    }
+}
---
+++ CustomFileCompression/CustomFileCompression/CustomFileCompression/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+namespace CustomFileCompression
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                    CompressFiles cf = new CompressFiles();
+
+
+                    var SourcePath = "/Users/pavan/Desktop/GitHub/CSharp/CustomFileCompression/Input";
+                    var DestinationPath = "/Users/pavan/Desktop/GitHub/CSharp/CustomFileCompression/Output";
+              double AgeOfFile = 60;
+
+            if (Directory.Exists(SourcePath) && Directory.Exists(DestinationPath))
+            {
+                cf.MoveCompressedFiles(SourcePath, DestinationPath, AgeOfFile);
+                Console.WriteLine("Files moved");
+            }
+            else { Console.WriteLine("No dir found!"); }
+        }
+    }
+}
