14b2bd67e205ad52639aa36a0d9c04a75c9e83d0
Pavan Kumar Mangalapally <reddy_546@hotmail.com>
2021-08-04 02:53:16 +0000

    Test Installer in VS 2019

---
+++ ConsoleApplicationWithInstallerPackage/ConsoleApplicationWithInstallerPackage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationWithInstallerPackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
+//https://www.youtube.com/watch?v=6Mfp2EXmVNI
+//https://stackoverflow.com/questions/3028263/changing-the-default-installation-path-in-windows-application
+//https://marketplace.visualstudio.com/items?itemName=VisualStudioClient.MicrosoftVisualStudio2017InstallerProjects
+//
